b257ac514fe8f5f9036e4e562c83fa400b90f578
SebastianBuftea <sebibuftea85@gmail.com>
2024-01-30 16:13:32 +0100

    [UPDATE] show in the page

--- index.php
+++ index.php
@@ -5,26 +5,28 @@
     public $year;
     public $genre;
     public $duration;
-    public $permitted_age;
 
+    /* definiamo la funzione construct  */
     function __construct($_name, $_year, $_genre, $_duration){
         $this->name = $_name;
         $this->year = $_year;
         $this->genre = $_genre;
         $this->duration = $_duration;   
     }
+
+    //definiamo un metodo
     public function showFilms(){
         return $this->name." è nei top migliori film";
     }
  };
 
  $movie_1= new Movie("Oppenheimer", 2023,"Thriller/Film di mistero","3 ore");
- 
-
 
  $movie_2= new Movie("Natale in Sudafrica", 2010,"Commedia","1h 45min");
 
- echo $movie_1->showFilms().' del '.$movie_1->year;
+ //mostriamo a schermo i due film
+ echo $movie_1->showFilms().' del '.$movie_1->year." con il suo genere ".$movie_1->genre." e la sua durata di ".$movie_1->duration." si è acaparrato un sacco di recensioni positive <br>";
+ echo $movie_2->name.' del '.$movie_2->year." dal genere ".$movie_2->genre." e dalla durata di ".$movie_2->duration." risulta un film ancora ad oggi amato dai italiani ";
 
 ?>
 
