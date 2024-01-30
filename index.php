53f8aa34d52fe1c5cdab16f55068b465f666e269
SebastianBuftea <sebibuftea85@gmail.com>
2024-01-30 15:43:04 +0100

    [ADD] construct

--- index.php
+++ index.php
@@ -1,7 +1,21 @@
 <?php
+//definizione classe movie
  class Movie{
     public $name;
     public $year;
+    public $genre;
 
+    function __construct($_name, $_year, $_genre){
+        $this->name = $_name;
+        $this->year = $_year;
+        $this->genre = $_genre;
+        
+    }
  };
+
+ $movie_1= new Movie();
+
+ $movie_2= new Movie();
+
 ?>
+
