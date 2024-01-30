12db9ba01b5abecca67ea87f1ffcd29dfa47ceb1
SebastianBuftea <sebibuftea85@gmail.com>
2024-01-30 16:04:32 +0100

    [ADD] Method

--- index.php
+++ index.php
@@ -4,18 +4,27 @@
     public $name;
     public $year;
     public $genre;
+    public $duration;
+    public $permitted_age;
 
-    function __construct($_name, $_year, $_genre){
+    function __construct($_name, $_year, $_genre, $_duration){
         $this->name = $_name;
         $this->year = $_year;
         $this->genre = $_genre;
-        
+        $this->duration = $_duration;   
+    }
+    public function showFilms(){
+        return $this->name." è nei top migliori film";
     }
  };
 
- $movie_1= new Movie();
+ $movie_1= new Movie("Oppenheimer", 2023,"Thriller/Film di mistero","3 ore");
+ 
 
- $movie_2= new Movie();
+
+ $movie_2= new Movie("Natale in Sudafrica", 2010,"Commedia","1h 45min");
+
+ echo $movie_1->showFilms().' del '.$movie_1->year;
 
 ?>
 
